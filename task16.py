592e9bde06e9ae47d5f15a58f2febf3baaf982e0
taalaybolotbekov <bolotbekovtaalay@gmail.com>
2020-03-10 13:17:59 +0600

    sixteen commit

---
+++ task16.py
@@ -0,0 +1,7 @@
+def mas():
+    massa = 79
+    for x in range(1,16):
+        everyear=1
+        massa=massa+everyear
+        return('Мой вес в луне %s будет каждый год %s ' %(massa ,x))
+print(mas())
